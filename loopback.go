b9c635b33aef409cdefe17592171aee76eac0cb5
Ben Pate <ben@pate.org>
2026-05-06 10:08:31 -0600

    Comments

--- loopback.go
+++ loopback.go
@@ -1,10 +1,11 @@
 package uri
 
-// IsLoopback returns TRUE if the provided domain is a loopback address (localhost, 127.0.0.1, ::1).
+// IsLoopback returns TRUE if the provided domain is a loopback address (localhost, 127.0.0.1, ::1)
 func IsLoopback(domain string) bool {
 	return (domain == "localhost") || (domain == "127.0.0.1") || (domain == "::1")
 }
 
+// NotLoopback returns TRUE if the provided domain is NOT a loopback address (otherserver.com, 192.168.0.5)
 func NotLoopback(domain string) bool {
 	return !IsLoopback(domain)
 }
