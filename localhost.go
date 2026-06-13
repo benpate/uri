ced4cc828bc57fae1fb921f3de5c928028110d26
Ben Pate <ben@pate.org>
2026-06-13 11:50:06 -0600

    Comprehensive "local" domain detection

--- localhost.go
+++ localhost.go
@@ -1,6 +1,9 @@
 package uri
 
-import "strings"
+import (
+	"net"
+	"strings"
+)
 
 // IsLocalURL returns TRUE if the URL contains a local hostname
 func IsLocalURL(url string) bool {
@@ -22,37 +25,35 @@ func NotLocalURL(url string) bool {
 // IsLocalHostname returns TRUE if the hostname is a local domain
 func IsLocalHostname(hostname string) bool {
 
-	// Private networks are defined by RFC 1918
-	// https://en.wikipedia.org/wiki/Private_network
+	// IPv4 private networks are defined by RFC 1918
+	// https://www.rfc-editor.org/rfc/rfc1918
+	// https://www.iana.org/assignments/iana-ipv4-special-registry/
+
+	// IPv6 private networks are defined by RFC 4193
+	// https://www.rfc-editor.org/rfc/rfc4193
+	// https://www.iana.org/assignments/iana-ipv6-special-registry/
 
 	// Normalize the hostname
 	hostname = strings.ToLower(hostname)
 
+	// Loopback addresses are always local
 	if IsLoopback(hostname) {
 		return true
 	}
 
+	// The .local domain is commonly used for local network devices and services, and is treated as local by many systems.
 	if strings.HasSuffix(hostname, ".local") {
 		return true
 	}
 
-	// 24-bit block
-	if strings.HasPrefix(hostname, "10.") {
-		return true
+	// Private IP ranges: RFC 1918 (IPv4) and RFC 4193 unique-local (IPv6)
+	if ip := net.ParseIP(hostname); ip != nil {
+		if ip.IsPrivate() {
+			return true
+		}
 	}
 
-	// 20-bit block
-	if strings.HasPrefix(hostname, "172.16") {
-		return true
-	}
-
-	// 16-bit block
-	if strings.HasPrefix(hostname, "192.168") {
-		return true
-	}
-
-	// TODO: IPv6 private networks
-
+	// Otherwise, the hostname is not local
 	return false
 }
 
